7c6d16d481c903841318381b4a1ebb7290b187ce
mattiabenelli <benellimattia0@gmail.com>
2022-11-28 17:40:03 +0100

    milestone 1

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+1) Chiedi all'utente di inserire il numero di chilometri che si vogliono percorrere
+2) Chiedi all'utente di inserire la propria età
+3) Calcola il prezzo del biglietto moltiplicando i chilometri da percorrere per 0.21€
+4) In base all'età del passeggero applica uno sconto
+    4.1) se minorenne applica sconto del 20% e se over 65 applica uno sconto del 40%
---
+++ js/script.js
@@ -0,0 +1,17 @@
+const buttonSubmit = document.getElementById('button-submit');
+
+buttonSubmit.addEventListener('click', function(){
+    let input_tag  = document.getElementById('name');
+    let value_input = input_tag.value;
+    console.log(value_input)
+});
+buttonSubmit.addEventListener('click', function(){
+    let input_tag  = document.getElementById('km');
+    let value_input = input_tag.value;
+    console.log(value_input)
+});
+buttonSubmit.addEventListener('click', function(){
+    let input_tag  = document.getElementById('age');
+    let value_input = input_tag.value;
+    console.log(value_input)
+});
