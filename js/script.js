ce72477b980360e67bf2c1fc543e9fe07f9afc36
mattiabenelli <benellimattia0@gmail.com>
2022-11-29 11:00:33 +0100

    fixed issue

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 const buttonSubmit = document.getElementById('button-submit');
 
 buttonSubmit.addEventListener('click', function(){
-    let input_tag  = document.getElementById('name');
-    let value_input = input_tag.value;
-    console.log(value_input)
-});
-buttonSubmit.addEventListener('click', function(){
-    let input_tag  = document.getElementById('km');
-    let value_input = input_tag.value;
-    console.log(value_input)
-});
-buttonSubmit.addEventListener('click', function(){
-    let input_tag  = document.getElementById('age');
-    let value_input = input_tag.value;
-    console.log(value_input)
-});
+    let name_surname  = document.getElementById('name').value
+    console.log(name_surname)
 
-buttonSubmit.addEventListener('click', function(){
-    let price = document.getElementById('km').value * 0.21;
+    let chilometri  = document.getElementById('km').value
+    console.log(chilometri)
+
+    let eta  = document.getElementById('age').value
+    console.log(eta)
+
+    let price = chilometri * 0.21;
     console.log(`il prezzo non scontato è ${price}`);
-    if(document.getElementById('age').value === "minorenne" ){
+    if(eta == 'minorenne' ){
         price = price - (price * 0.4);
         price = price.toFixed(2);
         console.log(`il prezzo scontato del 40% è ${price} €`);
     }
-    else if(document.getElementById('age').value === "over_65"){
+    else if(eta == 'over_65'){
         price = price - (price * 0.8);
         price = price.toFixed(2);
         console.log(`il prezzo scontato del 80% è ${price} €`);
@@ -34,5 +27,6 @@ buttonSubmit.addEventListener('click', function(){
         console.log(`il biglietto costa ${price} €`);    
     }
     document.getElementById("ticket-price").innerHTML = price + "$";
-    document.getElementById("name-surname").innerHTML = name ;
+    document.getElementById("name_surname").innerHTML = name_surname ;
 });
+
